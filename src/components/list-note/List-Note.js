71717b04098bbee34a1aab3002151d5161c79ecc
alabi sodiq <adisco4420@gmail.com>
2019-03-18 09:35:14 +0100

    edit note

--- src/components/list-note/List-Note.js
+++ src/components/list-note/List-Note.js
@@ -4,10 +4,8 @@ import Header from './../haeder/Header';
 class ListNotes extends Component {
 
 
-    editNote = (note) => {
-        const id = note.id;
-        let listNote = this.state.listAllNote;
-
+    editNote = (note, index) => {
+        this.setState({editTitle: note.title, editDesc: note.desc, editId: index})
     }
     deleteNote = (note) => {
         let listNote = this.state.listAllNote
@@ -19,14 +17,31 @@ class ListNotes extends Component {
     updateList = (item) => {
         this.setState({listAllNote: item})
     }
+    handleChange = e => {
+        e.preventDefault();
+        const { name, value} = e.target
+        this.setState({[name]: value})
+    }
+    handleSave = () => {
+        let title = this.state.editTitle;
+        let desc = this.state.editDesc;
+        let id = this.state.editId
+        let listNote = this.state.listAllNote;
+        listNote[id] = {id: id, title: title, desc: desc};
+        this.setState({listAllNote: listNote})
+        localStorage.setItem('list-note', JSON.stringify(listNote))
+    }
 
     constructor(props) {
         super(props)
 
         this.state = {
-            listAllNote : this.props.alLNoteProps
+            listAllNote : this.props.alLNoteProps,
+            editTitle: '',
+            editDesc: '',
+            editId: 0
         }
-        console.log(this.props.ListValue)
+        // console.log(this.props.ListValue)
     }
     render() {
         return (
@@ -49,7 +64,7 @@ class ListNotes extends Component {
                         <div className="card-footer">
                         <h5 className="card-title">{item.title}</h5> 
                         <p>{item.desc}</p>     
-                        <i onClick={() => this.editNote(item)} className="fa fa-edit mr-3 text-info"></i>
+                        <i  data-toggle="modal" onClick={() => {this.editNote(item,index)}} data-target="#editModal"className="fa fa-edit mr-3 text-info"></i>
                         <i onClick={() => this.deleteNote(item)} className="fa fa-trash  text-danger" ></i>
                         </div>
                     </div>
@@ -59,6 +74,36 @@ class ListNotes extends Component {
                 }          
 
                </div>
+
+
+
+
+
+<div className="modal fade" id="editModal" tabIndex="-1" role="dialog"  aria-hidden="true">
+  <div className="modal-dialog" role="document">
+    <div className="modal-content">
+      <div className="modal-header">
+    <input name="editTitle" defaultValue={this.state.editTitle} onChange={this.handleChange} 
+      style={{width: '40%'}} className="form-control"  />
+        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div className="modal-body">
+      <input className="form-control" style={{height: '60px' }} defaultValue={this.state.editDesc} 
+      name="editDesc" onChange={this.handleChange} />
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+        {
+            (!this.state.editTitle.length || !this.state.editDesc.length) ?  '' : 
+            <button type="button"  data-dismiss="modal" onClick={this.handleSave}  className="btn btn-success">Save
+           </button>
+        }
+      </div>
+    </div>
+  </div>
+</div>
        
             </div>
            </div>
